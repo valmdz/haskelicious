9c0f6df554d1f46e38dcd10ff633daa12ca8a8a3
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2018-04-01 17:47:08 +0200

    Today I learned:
    Data-types; lists - case-expressions
    Recursion
    Type-polymorphism - I can use lower-case letters to describe *any* type.

---
+++ Main.hs
@@ -0,0 +1,41 @@
+import Prelude hiding (reverse)
+import System.Environment
+
+-- A list can either be empty;
+--
+--   []
+--
+-- or an element `n` followed by another list `ns`;
+--
+--   (n:ns)
+--
+reverse :: [a] -> [a]
+reverse az = case az of
+  []       -> []
+  (x : xs) -> reverse xs ++ singleton x
+
+
+singleton :: a -> [a]
+singleton x = x : []
+
+-- {:)  :: Int   -> [Int] -> [Int]
+-- (++) :: [Int] -> [Int] -> [Int]
+
+exampleInput :: [Char]
+exampleInput = 'o':'c':'i':'r':'e':'d':'e':'f':[]
+-- aka:
+--     ['o','c','i','r','e','d','e','f']
+-- aka:
+--     "ociredef"
+
+main :: IO ()
+main =
+  print . reverse $ exampleInput
+
+-- reverse [1,2,3,3]
+
+-- az = 1 : (2 : 3 : 3 : [])
+-- x  = 1
+-- xs =      2 : 3 : 3 : []
+--
+-- result = 3 : 3 : 2 : 1 : []
